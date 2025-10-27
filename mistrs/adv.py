32738cec80627b68658b88a45febc6571046dd83
rstandage <57262746+rstandage@users.noreply.github.com>
2025-10-27 17:27:13 +0000

    clean up, added debug functions, and advanced API functions

---
+++ mistrs/adv.py
@@ -0,0 +1,15 @@
+import requests, json
+from urllib.parse import urlparse
+from mistrs import get_paginated, get, post
+
+#def get_sites(org_id, headers):
+    #create a dictionary of ids and sites
+
+#def get_inventory(org_id, headers, hwtype):
+    #get inventory based on hw type. returns an array
+
+#def upgrade_site():
+
+#def upgrade_ap(org_id, headers, mac, version):
+
+#def ping_hook():
